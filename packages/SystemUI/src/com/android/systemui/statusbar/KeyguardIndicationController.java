e22635e595fcfceef40539e0e5469ce12b3e2a94
minaripenguin <minaripenguin@users.noreply.github.com>
2023-12-17 08:36:37 +0800

    SystemUI: Implement bouncer face unlock animation
    mnri: move face indicator to top, rewrite code and fix inconsistencies
    
    Co-authored-by: jhenrique09 <jhsv09@gmail.com>
    Co-authored-by: someone5678 <someone5678@users.noreply.github.com>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/KeyguardIndicationController.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/KeyguardIndicationController.java
@@ -377,10 +377,16 @@ public class KeyguardIndicationController {
 
         mStatusBarStateListener.onDozingChanged(mStatusBarStateController.isDozing());
     }
+    
+    public void setIndicationAreaTop(ViewGroup indicationAreaTop) {
+        mFaceIconView = indicationAreaTop.findViewById(R.id.face_unlock_icon);
+        if (mFaceIconView != null) {
+            mFaceIconView.updateColor();
+        }
+    }
 
     public void setIndicationArea(ViewGroup indicationArea) {
         mIndicationArea = indicationArea;
-        mFaceIconView = indicationArea.findViewById(R.id.face_unlock_icon);
         mTopIndicationView = indicationArea.findViewById(R.id.keyguard_indication_text);
         mLockScreenIndicationView = indicationArea.findViewById(
             R.id.keyguard_indication_text_bottom);
@@ -880,6 +886,8 @@ public class KeyguardIndicationController {
             mFaceIconView.setState(FaceUnlockImageView.State.SUCCESS);
         } else if (TextUtils.equals(biometricMessage, mContext.getString(R.string.keyguard_face_failed))) {
             mFaceIconView.setState(FaceUnlockImageView.State.NOT_VERIFIED);
+        } else if (TextUtils.equals(biometricMessage, mContext.getString(R.string.face_unlock_recognizing))) {
+            mFaceIconView.setState(FaceUnlockImageView.State.SCANNING);
         }
 
         mBiometricMessage = biometricMessage;
@@ -906,21 +914,18 @@ public class KeyguardIndicationController {
     }
 
     private void showFaceUnlockRecognizingMessage() {
-        mFaceIconView.setVisibility(View.VISIBLE);
-        mFaceIconView.setState(FaceUnlockImageView.State.SCANNING);
-        showBiometricMessage(mContext.getResources().getString(
-                                    R.string.face_unlock_recognizing));
+        String faceUnlockMessage = mContext.getResources().getString(
+            R.string.face_unlock_recognizing);
+        showBiometricMessage(faceUnlockMessage);
     }
 
     private void hideFaceUnlockRecognizingMessage() {
-        if (mFaceIconView != null) {
-            mFaceIconView.setVisibility(View.GONE);
-        }
         String faceUnlockMessage = mContext.getResources().getString(
             R.string.face_unlock_recognizing);
         if (mBiometricMessage != null && mBiometricMessage == faceUnlockMessage) {
             mBiometricMessage = null;
             hideBiometricMessage();
+            mFaceIconView.setState(FaceUnlockImageView.State.HIDDEN);
         }
     }
 
@@ -1570,6 +1575,10 @@ public class KeyguardIndicationController {
                 hideFaceUnlockRecognizingMessage();
             }
             updateDeviceEntryIndication(false);
+            mFaceIconView.setDozing(mDozing);
+            if (mDozing) {
+                mFaceIconView.updateFaceIconState();
+            }
         }
     };
 
